3f0b5ae29a59e08fe51bf073c3e9403d3cbaa32e
tsiondev <tsiontadeledev@gmail.com>
2023-08-31 23:52:24 +0300

    sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -15,6 +15,7 @@ export interface Game {
     parent_platforms:{platform:Platform}[]
     metacritic:number
   }
+  
 
-const useGames=(gameQuery:GameQuery)=>useData<Game>('/games',{params:{genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id}},[gameQuery])
+const useGames=(gameQuery:GameQuery)=>useData<Game>('/games',{params:{genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id,ordering:gameQuery.sortOrder}},[gameQuery])
 export default useGames
